589641f54b22fb150d793187f12ec5ae6746adaa
otiedano <1392401799@qq.com>
2019-05-14 13:29:24 +0800

    pwa设置

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  presets: [
-    '@vue/app'
-  ]
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/myvue/'
+      : '/'
+  ,
+  pwa: {
+    iconPaths: {
+      favicon32: 'd-favicon.ico',
+      favicon16: 'd-favicon.ico',
+      appleTouchIcon: 'd-favicon.ico',
+      maskIcon: 'd-favicon.ico',
+      msTileImage: 'd-favicon.ico'
+    }
+  }
 }
